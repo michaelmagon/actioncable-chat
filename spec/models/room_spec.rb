c6a376655029f2e79ec7010558d2fb526519181d
michael francis <michael_magon@outlook.com>
2022-10-23 23:54:00 +0800

    utilities and spec

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id           :bigint           not null, primary key
+#  name         :string
+#  participants :integer          default([]), is an Array
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
